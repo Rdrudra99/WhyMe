4e998b04a05d16a07cf71fa184eea8ad898db71d
Rdrudra99 <rdrudra99.work@gmail.com>
2024-12-18 07:14:32 -0500

    push the code , fixed bug 🟢

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
-const eslintConfig = [
-  // Extending valid configurations
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-  // Adding rules manually
-  {
-    rules: {
-      'next/core-web-vitals': 'off',
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-      'react/no-children-prop': 'off', // Disable the rule globally
-    },
-  },
-];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript',],
+  }),
+]
 
 export default eslintConfig;
